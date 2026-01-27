886093721a09991d7caba5c695b13955ec4c0e39
Steve Phelps <79sphelps@gmail.com>
2026-01-27 10:32:44 -0800

    Updating my resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Resume-Steve-Phelps-Dec2025-Frontend.pdf";
+import pdf from "../../Assets/../Assets/Resume(Trimmed)-StevePhelps-Jan2026-Frontend.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import Loading from "./Loading";
